77c6c977d7c2df8b67bdd848beb03a3a6c9172f5
Josh Larson <joshua-github@ourclan.net>
2012-02-23 20:44:52 -0600

    This has all the code for the season

---
+++ SquareFinder.h
@@ -0,0 +1,19 @@
+#ifndef SQUAREFINDER_H
+#define SQUAREFINDER_H
+
+#include "vision.h"
+
+
+class SquareFinder : public VisionSpecifics
+{
+public:
+	SquareFinder();
+	~SquareFinder();
+	
+	void GetBestTargets(HSLImage *img, TargetReport* targets, int& count);
+	
+private:
+	Image* lumPlane;
+};
+
+#endif
